bd12cc493d020a81f6298abc6055365397e18fc2
thiagosng <thiagonunes026@gmail.com>
2021-10-13 11:35:53 -0300

    [FEAT] Novos campos e foreign key

--- backend/src/database/migrations/1633906662324-CreateProjects.ts
+++ backend/src/database/migrations/1633906662324-CreateProjects.ts
@@ -1,89 +1,74 @@
-import {MigrationInterface, QueryRunner, Table , TableForeignKey, TableIndex} from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+  TableIndex,
+} from 'typeorm';
 
-export class CreateProjects1633906662324 implements MigrationInterface {
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: 'projects',
-                columns: [
-                    {
-                        name: 'id',
-                        type: 'serial',
-                        isPrimary: true,
-                    },
-                    {
-                        name: 'name',
-                        type: 'varchar(255)',
-                        
-                    },
-                    {
-                        name: 'description',
-                        type: 'varchar(255)',
-                    },
-                    {
-                        name: 'start_date',
-                        type: 'varchar(255)',
-                    },
-                    {
-                        name: 'end_date',
-                        type: 'varchar(255)',
-                    },
-                    {
-                        name: 'duration',
-                        type: 'varchar(255)',
-                    },
-                    {
-                        name: 'user_id',
-                        type: 'int',
-                        isNullable: true,
-                    },
-                ],
-            }),
-            true,
-        );
-        await queryRunner.createIndex(
-            'projects',
-            new TableIndex({
-              name: 'idx_project_1',
-              columnNames: ['name'],
-            }),
-          );
-      
-          await queryRunner.createIndex(
-            'projects',
-            new TableIndex({
-              name: 'idx_project_2',
-              columnNames: ['description'],
-            }),
-          );
+export default class CreateProjects1633906662324 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'projects',
+        columns: [
+          {
+            name: 'id',
+            type: 'serial',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'description',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'start_date',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'end_date',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'duration',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+          },
+        ],
+      }),
+      true,
+    );
+    await queryRunner.createIndex(
+      'projects',
+      new TableIndex({
+        name: 'idx_project_1',
+        columnNames: ['name'],
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'projects',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
 
-          await queryRunner.createIndex(
-            'projects',
-            new TableIndex({
-              name: 'idx_project_3',
-              columnNames: ['start_date'],
-            }),
-          );
-
-          await queryRunner.createIndex(
-            'projects',
-            new TableIndex({
-              name: 'idx_project_4',
-              columnNames: ['end_date'],
-            }),
-          );
-
-          await queryRunner.createIndex(
-            'projects',
-            new TableIndex({
-              name: 'idx_project_5',
-              columnNames: ['end_date'],
-            }),
-          );
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('projects');
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('projects');
+  }
 }
