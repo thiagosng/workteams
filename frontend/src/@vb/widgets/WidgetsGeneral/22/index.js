26c54705d0e7c1edd6bbb0901dea941d4cb2b227
thiagosng <thiagonunes026@gmail.com>
2021-10-29 15:04:47 -0300

    [FEAT] Estilização do card header de projectsList

--- frontend/src/@vb/widgets/WidgetsGeneral/22/index.js
+++ frontend/src/@vb/widgets/WidgetsGeneral/22/index.js
@@ -4,16 +4,16 @@ import style from './style.module.scss'
 const General22 = () => {
   return (
     <div className="row">
-      <div className="col-lg-4">
+      <div className="col-lg-12">
         <div className={`${style.item} mb-xl-0 mb-3`}>
           <span className={style.icon}>
             <i className="fe fe-home" />
           </span>
           <div className={style.desc}>
-            <span className={style.title}>Block Title</span>
-            <p>Waiting for review</p>
+            <span className={style.title}>Seus Projetos</span>
+            <p>Pagina de exibição dos projetos</p>
           </div>
-          <div className={`${style.line} bg-success`} />
+          <div className={`${style.line} bg-primary`} />
         </div>
       </div>
     </div>
