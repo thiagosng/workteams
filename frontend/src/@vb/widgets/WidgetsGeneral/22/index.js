e21aea338595670098ea3651a082657bb4406e95
thiagosng <thiagonunes026@gmail.com>
2021-11-02 23:24:34 -0300

    [FEAT] Transformando o card em um botão para registro

--- frontend/src/@vb/widgets/WidgetsGeneral/22/index.js
+++ frontend/src/@vb/widgets/WidgetsGeneral/22/index.js
@@ -1,19 +1,34 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import style from './style.module.scss'
 
 const General22 = () => {
+  const history = useHistory()
+
+  const handleClick = () => {
+    history.push('/projects/register')
+  }
+
   return (
-    <div className="row">
-      <div className="col-lg-12">
-        <div className={`${style.item} mb-xl-0 mb-3`}>
-          <span className={style.icon}>
-            <i className="fe fe-plus-square" />
-          </span>
-          <div className={style.desc}>
-            <span className={style.title}>Seus Projetos</span>
-            <p>Pagina de exibição dos projetos</p>
+    <div className={`${style.card}`}>
+      <div className="row">
+        <div
+          className="col-lg-12"
+          onClick={handleClick}
+          onKeyDown={handleClick}
+          role="button"
+          tabIndex={0}
+        >
+          <div className={`${style.item} mb-xl-0 mb-3`}>
+            <span className={style.icon}>
+              <i className="fe fe-plus-square" />
+            </span>
+            <div className={style.desc}>
+              <span className={style.title}>Criar novo projeto</span>
+              <p>Clique aqui para criar um novo projeto</p>
+            </div>
+            <div className={`${style.line} bg-primary`} />
           </div>
-          <div className={`${style.line} bg-primary`} />
         </div>
       </div>
     </div>
