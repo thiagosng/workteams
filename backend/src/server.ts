56f7785bad8b475fa51a1bc8bbe12b99a5274d43
iago fagundes <iago.fagundes@outlook.com>
2021-11-05 09:08:29 -0300

    fix: replace bodyParse with native express modules

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,8 +9,8 @@ import routes from './routes';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 app.use(routes);
 
