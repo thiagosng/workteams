21d11a56929d2da9244c1e803c7ec782eca77041
thiagosng <thiagonunes026@gmail.com>
2021-11-08 00:15:05 -0300

    [FIX] Arrumada a migration de comentários

---
+++ backend/src/database/migrations/1636302869926-CreateComments.ts
@@ -0,0 +1,82 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateComments1636302869926 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comments',
+        columns: [
+          {
+            name: 'id',
+            type: 'serial',
+            isPrimary: true,
+          },
+          {
+            name: 'user_id',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'project_id',
+            type: 'int',
+            isNullable: true,
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'author',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'datetime',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+      true,
+    ); // true = force to create table
+
+    await queryRunner.createForeignKey(
+      'comments',
+      new TableForeignKey({
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'comments',
+      new TableForeignKey({
+        columnNames: ['project_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'projects',
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('comments', 'user_id');
+    await queryRunner.dropForeignKey('comments', 'project_id');
+    await queryRunner.dropTable('comments');
+  }
+}
