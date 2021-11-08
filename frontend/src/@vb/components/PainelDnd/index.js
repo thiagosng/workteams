337683a85c43c661ff322ee2f4d1b3cf43adf469
thiagosng <thiagonunes026@gmail.com>
2021-11-07 23:11:20 -0300

    [FEAT] Adicionado ReactDND e TodoList na home

---
+++ frontend/src/@vb/components/PainelDnd/index.js
@@ -0,0 +1,178 @@
+/* eslint-disable no-shadow */
+import React, { useState, useEffect } from 'react'
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import { Button } from 'antd'
+import { useHistory } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+
+const itemsFromBackend = [
+  { id: uuid(), content: 'First task' },
+  { id: uuid(), content: 'Second task' },
+  { id: uuid(), content: 'Third task' },
+  { id: uuid(), content: 'Fourth task' },
+  { id: uuid(), content: 'Fifth task' },
+]
+
+const columnsFromBackend = {
+  1: {
+    name: 'Cliente em Potencial',
+    items: itemsFromBackend,
+  },
+  2: {
+    name: 'Dados Confirmados',
+    items: [],
+  },
+  3: {
+    name: 'Reunião Agendada',
+    items: [],
+  },
+}
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return
+  const { source, destination } = result
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId]
+    const destColumn = columns[destination.droppableId]
+    const sourceItems = [...sourceColumn.items]
+    const destItems = [...destColumn.items]
+    const [removed] = sourceItems.splice(source.index, 1)
+    destItems.splice(destination.index, 0, removed)
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems,
+      },
+    })
+  } else {
+    const column = columns[source.droppableId]
+    const copiedItems = [...column.items]
+    const [removed] = copiedItems.splice(source.index, 1)
+    copiedItems.splice(destination.index, 0, removed)
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    })
+  }
+}
+
+function PainelDnd() {
+  const [columns, setColumns] = useState(columnsFromBackend)
+  const [leads, setLeads] = useState([])
+  const history = useHistory()
+
+  const newLead = () => {
+    history.push('/new-lead')
+  }
+
+  useEffect(() => {
+    setLeads(JSON.parse(localStorage.getItem('lead')))
+    setColumns([
+      {
+        name: 'Cliente em Potencial',
+        items: itemsFromBackend,
+      },
+      {
+        name: 'Dados Confirmados',
+        items: [],
+      },
+      {
+        name: 'Reunião Agendada',
+        items: [],
+      },
+    ])
+    localStorage.setItem('columns', columns)
+  }, [])
+
+  return leads ? (
+    <div style={{ display: 'flex', justifyContent: 'center', height: '100%' }}>
+      <div className="col-md-3">
+        <Button className="btn btn-primary btn-block" onClick={(e) => newLead(e)}>
+          Novo Lead (+)
+        </Button>
+      </div>
+      <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
+        {Object.entries(columns).map(([columnId, column]) => {
+          return (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+              key={columnId}
+            >
+              <div style={{ margin: 1 }}>
+                <h2>{column.name}</h2>
+                <Droppable droppableId={columnId} key={columnId}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                          background: snapshot.isDraggingOver ? '' : '',
+                          padding: 4,
+                          width: 300,
+                          minHeight: 500,
+                        }}
+                      >
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable key={item.id} draggableId={item.id} index={index}>
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={{
+                                      userSelect: 'none',
+                                      padding: 16,
+                                      margin: '0 0 8px 0',
+                                      minHeight: '50px',
+                                      backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                                      color: 'white',
+                                      ...provided.draggableProps.style,
+                                    }}
+                                  >
+                                    {item.content}
+                                  </div>
+                                )
+                              }}
+                            </Draggable>
+                          )
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    )
+                  }}
+                </Droppable>
+              </div>
+            </div>
+          )
+        })}
+      </DragDropContext>
+    </div>
+  ) : (
+    <div>
+      <h1>Nenhum lead cadastrado...</h1>
+      <span>
+        <Button className="btn btn-primary btn-block" onClick={(e) => newLead(e)}>
+          Novo Lead (+)
+        </Button>
+      </span>
+    </div>
+  )
+}
+
+export default PainelDnd
