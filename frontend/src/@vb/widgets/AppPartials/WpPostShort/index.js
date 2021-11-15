19e1b6b0245a2ad03d17c0cab05e4ff670fa1ab0
thiagosng <thiagonunes026@gmail.com>
2021-11-15 19:18:48 -0300

    [FEAT] Novas funcionalidades nos posts

--- frontend/src/@vb/widgets/AppPartials/WpPostShort/index.js
+++ frontend/src/@vb/widgets/AppPartials/WpPostShort/index.js
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { Menu, Dropdown, Pagination } from 'antd'
-import { getPostsData } from 'services/posts'
+import { Menu, Dropdown, Pagination, Modal } from 'antd'
+import { ExclamationCircleOutlined } from '@ant-design/icons'
+import { getPostsData, deletePost } from 'services/posts'
+import { useHistory } from 'react-router-dom'
 import moment from 'moment'
 
 const WpPostShort = () => {
   const [posts, setPosts] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
   const [postsPerPage] = useState(5)
-
-  useEffect(() => {
-    getPostsData().then((res) => {
-      setPosts(res)
-    })
-  }, [])
+  const [isDelete, setIsDelete] = useState(false)
+  const history = useHistory()
 
   const indexOfLastPost = currentPage * postsPerPage
   const indexOfFirstPost = indexOfLastPost - postsPerPage
@@ -27,58 +25,106 @@ const WpPostShort = () => {
     setPosts(post)
   }
 
-  const dropdownMenu = (
-    <Menu>
-      <Menu.Item>
-        <a>Editar</a>
-      </Menu.Item>
-      <Menu.Item>
-        <a>Deletar</a>
-      </Menu.Item>
-    </Menu>
-  )
+  const postagens = currentPosts.map((post) => {
+    // const GoToEdit = () => {
+    //   history.push(`/dashboard/update/${post.id}`)
+    // }
+
+    console.log('post', post.id)
+
+    function showDeleteConfirm() {
+      Modal.confirm({
+        title: 'Você deseja mesmo deletar esse usuário ?',
+        icon: <ExclamationCircleOutlined />,
+        content: 'O usuário será deletado de forma permanente',
+        okText: 'Deletar',
+        okType: 'danger',
+        cancelText: 'Cancelar',
+        onOk() {
+          try {
+            deletePost(post.id)
+            setIsDelete(true)
+          } catch (error) {
+            console.log(error)
+          }
+        },
+        onCancel() {
+          console.log('Cancel')
+        },
+      })
+    }
+    return (
+      <div>
+        <div className="card">
+          <div className="card-body">
+            <div className="text-right">
+              <Dropdown
+                overlay={
+                  <Menu>
+                    <Menu.Item>
+                      <a
+                        onClick={() => history.push(`/posts/update/${post.id}`)}
+                        onKeyPress={() => history.push(`/posts/update/${post.id}`)}
+                        role="button"
+                        tabIndex={0}
+                      >
+                        Editar
+                      </a>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a
+                        onClick={showDeleteConfirm}
+                        onKeyPress={showDeleteConfirm}
+                        role="button"
+                        tabIndex={0}
+                      >
+                        Deletar
+                      </a>
+                    </Menu.Item>
+                  </Menu>
+                }
+                placement="bottomRight"
+              >
+                <button type="button" className="btn btn-light">
+                  <i className="fe fe-more-vertical" />
+                </button>
+              </Dropdown>
+            </div>
+            <div className="mb-2">
+              <a
+                href="#"
+                onClick={(e) => e.preventDefault()}
+                className="text-dark font-size-24 font-weight-bold"
+              >
+                {post.title}
+              </a>
+            </div>
+            <div className="mb-3">
+              <a className="font-weight-bold" href="#" onClick={(e) => e.preventDefault()}>
+                {post.createdBy}
+              </a>{' '}
+              em {moment(post.createdAt).format('YYYY-MM-DD 00:00:00')}
+            </div>
+            <div>
+              <p>{post.content}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  })
 
   useEffect(() => {
+    getPostsData().then((res) => {
+      setPosts(res)
+    })
     getAllPosts()
-  }, [])
+    setIsDelete(false)
+  }, [isDelete])
 
   return (
     <div className="wp-post-short">
-      <div className="wp-post-short__list">
-        {currentPosts.map((post) => (
-          <div>
-            <div className="card">
-              <div className="card-body">
-                <div className="text-right">
-                  <Dropdown overlay={dropdownMenu} placement="bottomRight">
-                    <button type="button" className="btn btn-light">
-                      <i className="fe fe-more-vertical" />
-                    </button>
-                  </Dropdown>
-                </div>
-                <div className="mb-2">
-                  <a
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                    className="text-dark font-size-24 font-weight-bold"
-                  >
-                    {post.title}
-                  </a>
-                </div>
-                <div className="mb-3">
-                  <a className="font-weight-bold" href="#" onClick={(e) => e.preventDefault()}>
-                    {post.createdBy}
-                  </a>{' '}
-                  Post publicado em {moment(post.createdAt).format('YYYY-MM-DD 00:00:00')}
-                </div>
-                <div>
-                  <p>{post.content}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className="wp-post-short__list">{postagens}</div>
       <div className="wp-post-short__pagination">
         <Pagination
           defaultCurrent={1}
--- frontend/src/@vb/widgets/Forms/FormAddPost/index.js
+++ frontend/src/@vb/widgets/Forms/FormAddPost/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Editor } from 'react-draft-wysiwyg'
-import { Input, Form, notification } from 'antd'
+import { Input, Form, notification, Spin } from 'antd'
 import { useHistory, useParams } from 'react-router-dom'
-import { SmileOutlined } from '@ant-design/icons'
-import { createPost } from 'services/posts'
+import { SmileOutlined, LoadingOutlined } from '@ant-design/icons'
+import { createPost, updatePost, getPostsDataId } from 'services/posts'
 import { connect } from 'react-redux'
 
 const mapStateToProps = ({ user }) => ({ user })
@@ -11,26 +11,38 @@ const mapStateToProps = ({ user }) => ({ user })
 const FormAddPost = ({ user }) => {
   const [post, setPost] = useState({
     title: '',
-    content: 'teste',
+    content: '',
     createdBy: user.name,
   })
-
+  const [isLoading, setIsLoading] = useState(false)
+  const [updatePosts, setUpdatePost] = useState('')
   const { id } = useParams()
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />
 
   const history = useHistory()
 
-  const onEditorStateChange = (editorState) => {
-    setPost({
-      ...post,
-      content: editorState.blocks.map((block) => block.text).join('\n'),
-    })
+  const getPostsId = async () => {
+    try {
+      const posts = await getPostsDataId(id)
+      setUpdatePost(posts)
+      if (posts) {
+        setIsLoading(false)
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
-  const onChange = (e) => {
-    setPost({
-      ...post,
-      title: e.target.value,
-    })
+  const editPost = async () => {
+    try {
+      const response = await updatePost(id, updatePosts)
+      if (!response.error) {
+        openNotification()
+        history.push('/dashboard')
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   const openNotification = () => {
@@ -54,14 +66,36 @@ const FormAddPost = ({ user }) => {
     }
   }
 
+  useEffect(() => {
+    setIsLoading(true)
+    if (id) {
+      getPostsId()
+    } else {
+      setIsLoading(false)
+    }
+  }, [])
+
   console.log('posts', post)
-  return (
+  console.log('updatePosts', updatePosts)
+  return !isLoading ? (
     <Form layout="vertical">
       <div className="row">
         <div className="col-md-6">
           <div className="form-group">
             <Form.Item name="title" label="Titulo">
-              <Input placeholder="Post title" defaultValue={post.title} onChange={onChange} />
+              <Input
+                placeholder="Post title"
+                defaultValue={post.title}
+                onChange={
+                  id
+                    ? (e) => {
+                        setUpdatePost({ ...updatePosts, title: e.target.value })
+                      }
+                    : (e) => {
+                        setPost({ ...post, title: e.target.value })
+                      }
+                }
+              />
             </Form.Item>
           </div>
         </div>
@@ -74,7 +108,21 @@ const FormAddPost = ({ user }) => {
               height: 250,
               overflow: 'auto',
             }}
-            onChange={onEditorStateChange}
+            onChange={
+              id
+                ? (editorState) => {
+                    setUpdatePost({
+                      ...updatePosts,
+                      content: editorState.blocks.map((block) => block.text).join('\n'),
+                    })
+                  }
+                : (editorState) => {
+                    setPost({
+                      ...post,
+                      content: editorState.blocks.map((block) => block.text).join('\n'),
+                    })
+                  }
+            }
           />
         </Form.Item>
       </div>
@@ -82,7 +130,7 @@ const FormAddPost = ({ user }) => {
         <button
           type="submit"
           className="btn btn-success btn-with-addon text-nowrap"
-          onClick={newPost}
+          onClick={id ? editPost : newPost}
         >
           <span className="btn-addon">
             <i className="btn-addon-icon fe fe-plus-circle" />
@@ -91,6 +139,8 @@ const FormAddPost = ({ user }) => {
         </button>
       </Form.Item>
     </Form>
+  ) : (
+    <Spin indicator={antIcon} />
   )
 }
 
